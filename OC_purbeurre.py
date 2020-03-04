9e8ea66e45b51903a1ad252e36a570954bc04e1a
Remi TAUVEL <tauvel.remi@gmail.com>
2020-03-04 04:17:33 +0100

    interface 1er écran

--- OC_purbeurre.py
+++ OC_purbeurre.py
@@ -1,13 +1,53 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen, ScreenManager
+import mocks
 kivy.require("1.11.1")
 
 
-class EpicApp(App):
+class FirstScreen(Screen):
+    def update_db(self):
+        print('ici il faudra appeler ./python\ scripts/dbcreate.py')
+
+
+class CategoriesScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class CategoriesButton(Button):
+
+    def __init__(self, name, **kwargs):
+        super().__init__(**kwargs)
+        self.name = StringProperty(name)
+
+
+class FoodsScreen(Screen):
+    pass
+
+
+class ItemsScreen(Screen):
+    pass
+
+
+class FavouritesScreen(Screen):
+    pass
+
+
+class PurBeurreApp(App):
     def build(self):
-        return Label(text="hello world")
+        Builder.load_file('PurBeurre.kv')
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(FirstScreen(name='accueil'))
+        screen_manager.add_widget(CategoriesScreen(name='categories'))
+        screen_manager.add_widget(FoodsScreen(name='foods'))
+        screen_manager.add_widget(ItemsScreen(name='items'))
+        screen_manager.add_widget(FavouritesScreen(name='favourites'))
+        return screen_manager
 
 
 if __name__ == "__main__":
-    EpicApp().run()
+    PurBeurreApp().run()
---
+++ mocks.py
@@ -0,0 +1,52 @@
+CATEGORIES = [
+    {
+        'name': 'frais',
+        id: 1
+    },
+    {
+        'name': 'surgelé',
+        'id': 2
+    },
+    {
+        'name': 'liquide',
+        'id': 3
+    }
+]
+
+FOODS = [
+    {
+        'id': 1,
+        'name': 'roquefort',
+        'category_id': 1
+    },
+    {
+        'id': 2,
+        'name': 'chevre',
+        'category_id': 1
+    },
+    {
+        'id': 3,
+        'name': 'sachet de 10 cordons bleus surgelés',
+        'category_id': 2
+    },
+    {
+        'id': 4,
+        'name': 'Coquilles St-Jacques',
+        'category_id': 2
+    },
+    {
+        'id': 5,
+        'name': 'ravioles 250g',
+        'category_id': 2
+    },
+    {
+        'id': 6,
+        'name': 'fanta 1.5L',
+        'category_id': 3
+    },
+    {
+        'id': 7,
+        'name': 'coca 1.5L',
+        'category_id': 3
+    }
+]
